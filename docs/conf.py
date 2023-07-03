b273b77c4dd244e5724bd4129d0eacc09faf9557
Marc Finzi <mfinzi@hmc.edu>
2023-07-03 19:03:27 -0400

    update to conf

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,11 @@ release = '0.1.0'
 language = "en"
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'nbsphinx',
+    'sphinx.ext.mathjax',
     'recommonmark',
+    'sphinx.ext.intersphinx',
     # 'sphinx_rtd_dark_mode',
 ]
 
@@ -15,8 +18,25 @@ exclude_patterns = [
     '_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', 'notebooks/.ipynb_checkpoints'
 ]
 
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.ipynb', '.md']
 
 html_theme = 'sphinx_rtd_theme'
 # html_theme = 'sphinx_rtd_theme_dark_mode'
 html_static_path = ['_static']
+autosummary_generate = True
+napolean_use_rtype = False
+nbsphinx_execute = 'never'
+nbsphinx_codecell_lexer = 'ipython3'
+nbsphinx_prolog = r"""
+{% set docname = 'docs/notebooks/colabs/' + env.doc2path(env.docname, base=None).split('/')[-1] %}
+
+.. only:: html
+
+    .. role:: raw-html(raw)
+        :format: html
+
+    .. nbinfo::
+        Interactive online version:
+        :raw-html:`<a href="https://colab.research.google.com/github/wilson-labs/cola/blob/master/{{ docname }}"><img alt="Open In Colab" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a>`
+
+"""
