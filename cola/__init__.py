d26b4f83aac7a347ce6c8e0d8a1f76dd16c39d86
Marc Finzi <mfinzi@hmc.edu>
2023-07-05 18:39:16 -0400

    update to global init

--- cola/__init__.py
+++ cola/__init__.py
@@ -1,28 +1,8 @@
 __version__ = '0.1.0'
-# from .ops import LinearOperator
-# import pkgutil
-# from cola.utils import import_from_all
-# __all__ = []
+from cola.utils import import_from_all
+__all__ = []
 # for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
-#     import_from_all(module_name, globals(), __all__,__name__)
-import cola.ops
-from .basic_operations import *
-from .linalg import *
-# from .ops import *
-from .utils import *
-# import cola.ops
-#  from .ops import LinearOperator
-#  from .ops import *
-#  try:
-#      import jax
-#      # walk through each of the ops in .ops and register them
-#      # with jax.tree_util.register_pytree_node_class()
-#      from . import ops
-#      for name in dir(ops):
-#          op = getattr(ops, name)
-#          print(f"Registered {op} with JAX")
-#          if isinstance(op, LinearOperator):
-#              jax.tree_util.register_pytree_node_class(op)
-#
-#  except ImportError:
-#      pass
+import_from_all("basic_operations", globals(), __all__,__name__)
+import_from_all("linalg", globals(), __all__,__name__)
+import_from_all("utils", globals(), __all__,__name__)
+# import_from_all("ops", globals(), __all__,__name__)
