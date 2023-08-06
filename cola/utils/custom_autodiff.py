7e25963b9012d4511d7109a0e0263b1cc252c6f5
Marc Finzi <mfinzi@hmc.edu>
2023-08-06 19:43:02 -0400

    annotate iterative_autograd

--- cola/utils/custom_autodiff.py
+++ cola/utils/custom_autodiff.py
@@ -2,17 +2,38 @@ from functools import wraps
 
 
 def iterative_autograd(iterative_bwd):
+    """ Autograd wrapper for iterative solvers like CG, Lanczos, SLQ, etc.
+        Will construct a custom autograd rule for the iterative solver on every call
+        of the function.
+
+        Args:
+            iterative_bwd (Callable): The vjp for the iterative solver.
+            Should map ((params, output), d_output, unflatten, *args, **kwargs) -> d_A
+            (Shouldn't it be (d_output, *args, **kwargs) -> d_A?) )
+        
+        Usage:
+        
+        @iterative_autograd(iterative_bwd)
+        def iterative_fwd(A, *args, **kwargs):
+            # do stuff with A
+            # return output
+
+        """
     def wrap_iterative(iterative_fn):
+        """ Inner level wrapper """
         @wraps(iterative_fn)
         def iterative_w_A_arg(A, *args, **kwargs):
+            """ Version of iterative_fn that takes A as first argument and has a custom autograd rule."""
             par, unflatten = A.flatten()
 
+            # construct a fwd function that maps params -> (output, (params, output))
             def fwd(params):  # params -> y
                 A = unflatten(params)
                 output = iterative_fn(A, *args, **kwargs)
                 res = (params, output)
                 return output, res
 
+            # construct a bwd function that maps (params, output), doutput -> dparams
             def bwd(res, d_ouputs):  # dy -> dparams
                 # TODO: gradients for all of solver
                 dA, *_ = iterative_bwd(res, d_ouputs, unflatten, *args, **kwargs)
