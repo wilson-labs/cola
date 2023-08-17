d4ab446e3b9148f2fd28a5306d50fd8cc0812843
AndPotap <AndPotap@github.com>
2023-08-17 15:18:29 -0400

    Added LOBPCG

---
+++ cola/algorithms/lobpcg.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.sparse.linalg import LinearOperator as LO
+from scipy.sparse.linalg import lobpcg as lobpcg_sp
+from cola.ops import LinearOperator
+from cola.ops import Array
+from cola.ops import Dense
+from cola.utils import export
+
+
+@export
+def lobpcg(A: LinearOperator, start_vector: Array = None, max_iters: int = 100, tol: float = 1e-7,
+           pbar: bool = False):
+    xnp = A.xnp
+    del pbar, start_vector, tol
+
+    def matvec(x):
+        X = xnp.array(x, dtype=A.dtype)
+        out = A @ X
+        return np.array(out, dtype=np.float32)
+
+    A2 = LO(shape=A.shape, dtype=np.float32, matvec=matvec)
+    k = min(A.shape[0] - 1, max_iters)
+    X = np.random.normal(size=(A.shape[0], k)).astype(np.float32)
+    eigvals, eigvecs = lobpcg_sp(A2, X)
+    eigvals = xnp.array(np.copy(eigvals), dtype=A.dtype)
+    eigvecs = xnp.array(np.copy(eigvecs), dtype=A.dtype)
+    idx = xnp.argsort(eigvals, axis=-1)
+    info = {}
+    return eigvals[idx], Dense(eigvecs[:, idx]), info
