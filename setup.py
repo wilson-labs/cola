a8d93c5b70849271ebbebc58515885eaa2d8fa90
AndPotap <apotapczynski@gmail.com>
2023-08-24 20:42:44 -0400

    Modified setup.py to indicate that we are alpha

--- setup.py
+++ setup.py
@@ -22,16 +22,15 @@ setup(
         'dev': ['pytest', 'pytest-cov'],
     },
     packages=find_packages(),
-    # long_description=open('../README.md').read(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/wilson-labs/cola',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence :: Machine Learning',
     ],
     keywords=[
         'linear algebra',
@@ -39,5 +38,6 @@ setup(
         'sparse',
         'PDE',
         'AI',
+        'ML',
     ],
 )
