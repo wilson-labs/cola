cb54810c364036cdc418c6ccd2dfee883d8735d8
AndPotap <apotapczynski@gmail.com>
2023-09-11 17:58:04 -0400

    Added assert statement

--- tests/linalg/test_logdet.py
+++ tests/linalg/test_logdet.py
@@ -5,8 +5,9 @@ from cola.linalg import logdet
 from cola.ops import LinearOperator
 from cola.utils_test import parametrize, relative_error
 
-# tests should be passing now
-@parametrize(['torch', 'jax'], ['float64'], op_names).excluding[:,:,['psd_identity','psd_scalarmul']]
+
+@parametrize(['torch', 'jax'], ['float64'], op_names).excluding[:, :,
+                                                                ['psd_identity', 'psd_scalarmul']]
 def test_logdet(backend, precision, op_name):
     operator = get_test_operator(backend, precision, op_name)
     A, _, xnp = operator, operator.dtype, operator.xnp
@@ -23,7 +24,7 @@ def test_logdet(backend, precision, op_name):
         e2 = relative_error(l0, l2)
         assert e2 < 3 * tol, f"Dense logdet failed on {type(A)} with error {e2}"
     diag = xnp.diag(Adense)
-    not_scalarmul = relative_error(xnp.diag(diag.mean() + 0. * diag), Adense) > 1e-5
+    assert relative_error(xnp.diag(diag.mean() + 0. * diag), Adense) > 1e-5
     A3 = cola.PSD(A2) if A.isa(cola.PSD) else A2
     l3 = logdet(A3, tol=tol, method='iterative-stochastic', vtol=3e-2)
     e3 = relative_error(l0, l3)
