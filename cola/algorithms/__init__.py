0f92e506b65fe7b10da6093e4b6f0661b5ba9834
mfinzi <mfinzi@hmc.edu>
2023-09-20 14:20:20 -0400

    moving files around

--- cola/algorithms/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Low-level algorithms used in CoLA (no dispatch rules). """
-import pkgutil
-from cola.utils import import_from_all
-
-__all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    import_from_all(module_name, globals(), __all__, __name__)
