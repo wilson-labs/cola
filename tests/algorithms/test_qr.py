dd8bbbcd917d5ac3fabe194106e86995324b8443
AndPotap <apotapczynski@gmail.com>
2023-11-03 16:47:14 -0400

    Added additional test on QR shifted decomposition

--- tests/algorithms/test_qr.py
+++ tests/algorithms/test_qr.py
@@ -3,21 +3,35 @@ from cola.utils.test_utils import generate_spectrum, generate_pd_from_diag
 from cola.utils.test_utils import relative_error
 
 
-def test_shifted_qr():
+def test_shifted_qr_np():
     np.set_printoptions(formatter={"float": "{:0.2f}".format})
     np_dtype = np.float64
     N = 10
     diag = generate_spectrum(coeff=0.5, scale=1.0, size=N, dtype=np_dtype)
     A = generate_pd_from_diag(diag, dtype=diag.dtype, seed=21)
 
-    A_new = shifted_qr_np(A, shifts=np.zeros((50, )))
-    rel_error = relative_error(np.diag(A_new), diag)
+    A_new, _ = shifted_qr_np(A, shifts=np.zeros((50, )))
+    approx = np.sort(np.diag(A_new))[::-1]
+    rel_error = relative_error(approx, diag)
+    assert rel_error < 1e-8
+
+    A_new, Q = shifted_qr_np(A, shifts=diag[5:])
+    approx = Q @ A_new @ Q.T
+    rel_error = relative_error(approx, A)
+    assert rel_error < 1e-12
+
+    A_new, _ = shifted_qr_np(A, shifts=np.zeros((50, )))
+    approx = np.sort(np.diag(A_new))[::-1][:5]
+    rel_error = relative_error(approx, diag[:5])
     assert rel_error < 1e-8
 
 
 def shifted_qr_np(A, shifts):
     N = A.shape[0]
+    V, Id = np.eye(N), np.eye(N)
+
     for j in range(len(shifts)):
-        Q, R = np.linalg.qr(A - shifts[j] * np.eye(N), mode="complete")
-        A = R @ Q + shifts[j] * np.eye(N)
-    return A
+        Q, R = np.linalg.qr(A - shifts[j] * Id, mode="complete")
+        A = R @ Q + shifts[j] * Id
+        V @= Q
+    return A, V
