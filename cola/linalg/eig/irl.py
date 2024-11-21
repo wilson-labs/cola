44555a6363c99aa0a8bb40cd7d7a029bc8395980
AndPotap <apotapczynski@gmail.com>
2024-11-20 19:55:59 -0500

    Updated iram and irl

--- cola/linalg/eig/irl.py
+++ cola/linalg/eig/irl.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.sparse.linalg import LinearOperator as LO
-from scipy.sparse.linalg import eigs
+from scipy.sparse.linalg import eigsh
 
 from cola.ops import Array, Dense, LinearOperator
 from cola.utils import export
@@ -8,8 +8,8 @@ from cola.utils.utils_linalg import get_numpy_dtype
 
 
 @export
-def iram(A: LinearOperator, start_vector: Array = None, eig_n: int = 6, max_iters: int = 100, tol: float = 1e-7,
-         pbar: bool = False):
+def irl(A: LinearOperator, start_vector: Array = None, eig_n: int = 6, max_iters: int = 100, tol: float = 1e-7,
+        which: str = "LM", pbar: bool = False):
     xnp = A.xnp
     np_dtype = get_numpy_dtype(A.dtype)
     max_iters = None if max_iters == -1 else max_iters
@@ -24,7 +24,7 @@ def iram(A: LinearOperator, start_vector: Array = None, eig_n: int = 6, max_iter
         return np.array(out, dtype=np_dtype)
 
     A2 = LO(shape=A.shape, dtype=np_dtype, matvec=matvec)
-    eigvals, eigvecs = eigs(A2, k=eig_n, M=None, sigma=None, which="LM", v0=v0, tol=tol)
+    eigvals, eigvecs = eigsh(A2, k=eig_n, M=None, sigma=None, which=which, v0=v0, tol=tol, maxiter=max_iters)
     eigvals = xnp.array(eigvals, dtype=A.dtype, device=A.device)
     eigvecs = xnp.array(eigvecs, dtype=A.dtype, device=A.device)
     info = {}
