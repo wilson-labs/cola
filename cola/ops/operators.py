0d0c17054fc4d5bc92bc4bdd5384d8dcfe568a44
AndPotap <apotapczynski@gmail.com>
2025-01-20 19:35:04 -0500

    Applies cpu selectively

--- cola/ops/operators.py
+++ cola/ops/operators.py
@@ -421,8 +421,7 @@ class Sliced(LinearOperator):
     def __init__(self, A, slices):
         self.A = A
         self.slices = slices
-        if hasattr(slices[0], "device"):
-            slices = (slices[0].cpu(),) + (slices[1].cpu(),)
+        slices = tuple([sl.cpu() if hasattr(sl, "device") else sl for sl in slices])
         new_shape = np.arange(A.shape[0])[slices[0]].shape + np.arange(A.shape[1])[slices[1]].shape
         super().__init__(dtype=A.dtype, shape=new_shape)
         self.device = A.device
