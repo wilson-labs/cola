cd2779e2d20c9e250f34ee0465b485031308abc6
AndPotap <apotapczynski@gmail.com>
2025-01-25 07:07:10 -0500

    Added two versions of logdet quad

--- cola/linalg/tbd/logdet_quad.py
+++ cola/linalg/tbd/logdet_quad.py
@@ -3,18 +3,35 @@ from torch.autograd import Function
 from torch.func import vjp
 
 
-class LogdetQuad(Function):
+class LogdetQuadAccurate(Function):
     @staticmethod
     def forward(ctx, A, rhs):
-        out, A, L, soln = logdet_quad_fwd(A, rhs)
+        out, logdet, quad, A, L, soln = logdet_quad_fwd(A, rhs)
         ctx.save_for_backward(A, L, soln)
-        return out
+        return out, logdet, quad
 
     @staticmethod
     def backward(ctx, *grads):
         A, L, soln = ctx.saved_tensors
-        output = logdet_quad_bwd(grads, A, L, soln)
-        return output
+        out = logdet_quad_accurate_bwd(grads, A, L, soln)
+        return out
+
+
+logdet_quad_accurate = LogdetQuadAccurate.apply
+
+
+class LogdetQuad(Function):
+    @staticmethod
+    def forward(ctx, A, rhs):
+        out, logdet, quad, A, L, soln = logdet_quad_fwd(A, rhs)
+        ctx.save_for_backward(A, L, soln)
+        return out, logdet, quad
+
+    @staticmethod
+    def backward(ctx, *grads):
+        A, L, soln = ctx.saved_tensors
+        out = logdet_quad_bwd(grads, A, L, soln)
+        return out
 
 
 logdet_quad = LogdetQuad.apply
@@ -23,19 +40,34 @@ logdet_quad = LogdetQuad.apply
 def logdet_quad_fwd(A, rhs):
     L = torch.linalg.cholesky(A)
     logdet = 2.0 * torch.sum(torch.log(torch.diag(L)))
-    soln = solve_chol(L, rhs)
+    soln = chol_solve(L, rhs)
     quad = torch.sum(rhs * soln)
     out = logdet + quad
-    return out, A, L, soln
+    return out, logdet, quad, A, L, soln
 
 
 def logdet_quad_bwd(grads, A, L, soln):
     num_samples = 100
     probes = torch.randn(A.shape[1], num_samples, dtype=A.dtype, device=A.device)
     coef = 1.0 / probes.shape[-1]
-    # probes = torch.eye(A.shape[1], dtype=A.dtype, device=A.device)
-    # coef = 1.0
-    soln_probes = solve_chol(L, probes)
+    soln_probes = chol_solve(L, probes)
+    all_soln = torch.concatenate((soln, soln_probes), dim=-1)
+    all_rhs = torch.concatenate((-soln, coef * probes), dim=-1)
+    g = grads[0]
+    d_solves = g * all_soln
+
+    def fun(theta):
+        return theta @ all_rhs
+
+    dA = vjp_derivs(fun=fun, primals=A, duals=d_solves)
+    out = dA + tuple([None] * 3)
+    return out
+
+
+def logdet_quad_accurate_bwd(grads, A, L, soln):
+    probes = torch.eye(A.shape[1], dtype=A.dtype, device=A.device)
+    coef = 1.0
+    soln_probes = chol_solve(L, probes)
     all_soln = torch.concatenate((soln, soln_probes), dim=-1)
     all_rhs = torch.concatenate((-soln, coef * probes), dim=-1)
     g = grads[0]
@@ -55,7 +87,7 @@ def vjp_derivs(fun, primals, duals):
     return output
 
 
-def solve_chol(L, rhs):
+def chol_solve(L, rhs):
     y = torch.linalg.solve_triangular(L, rhs, upper=False)
     soln = torch.linalg.solve_triangular(L.T, y, upper=True)
     return soln
--- torch_logdet_quad_test.py
+++ torch_logdet_quad_test.py
@@ -1,20 +1,24 @@
 import numpy as np
 import torch
 
-from cola.linalg.tbd.logdet_quad import logdet_quad
+from cola.linalg.tbd.logdet_quad import logdet_quad, logdet_quad_accurate
 from cola.utils.utils_for_tests import generate_pd_from_diag, generate_spectrum
 
-dtype = torch.float32
+dtype = torch.float64
 device = "cpu"
 N, M = 13, 1
 diag = generate_spectrum(coeff=0.75, scale=1.0, size=N, dtype=np.float32)
 A = torch.tensor(generate_pd_from_diag(diag, dtype=diag.dtype), dtype=dtype, device=device)
 rhs = torch.ones(N, M, dtype=dtype, device=device)
 
-A = A.requires_grad_(True)
-P = torch.eye(A.shape[0], dtype=A.dtype, device=A.device)
-vtol, max_iters, tol = 0.1, 10, 1e-4
-out = logdet_quad(A, rhs)
+A = A.clone().detach().requires_grad_(True)
+out, *_ = logdet_quad(A, rhs)
+out.backward()
+print(out)
+print(A.grad.shape)
+
+A = A.clone().detach().requires_grad_(True)
+out, *_ = logdet_quad_accurate(A, rhs)
 out.backward()
 print(out)
 print(A.grad.shape)
